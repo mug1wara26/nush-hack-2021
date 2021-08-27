1f1b15f2e6a70474ebd80116cc62a41598146e4d
Spongiforma <56219994+Spongiforma@users.noreply.github.com>
2021-08-27 21:57:47 +0800

    temp camerax stuff

--- app/src/main/java/com/example/nush_hack21/ui/image/ImageFragment.kt
+++ app/src/main/java/com/example/nush_hack21/ui/image/ImageFragment.kt
@@ -1,12 +1,22 @@
 package com.example.nush_hack21.ui.image
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.os.Build
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.camera.core.ImageCapture
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.nush_hack21.R
+import java.io.File
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class ImageFragment : Fragment() {
 
@@ -16,6 +26,11 @@ class ImageFragment : Fragment() {
 
     private lateinit var viewModel: ImageViewModel
 
+    private var imageCapture: ImageCapture? = null
+
+    private lateinit var outputDirectory: File
+    private lateinit var cameraExecutor: ExecutorService
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -23,10 +38,48 @@ class ImageFragment : Fragment() {
         return inflater.inflate(R.layout.image_fragment, container, false)
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Request camera permissions
+        startCamera()
+
+
+        // Set up the listener for take photo button
+//        camera_capture_button.setOnClickListener { takePhoto() }
+//
+//        outputDirectory = getOutputDirectory()
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+    }
+
+    private fun startCamera() {}
+
+//    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+//        ContextCompat.checkSelfPermission(
+//            baseContext, it) == PackageManager.PERMISSION_GRANTED
+//    }
+
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(ImageViewModel::class.java)
         // TODO: Use the ViewModel
+
     }
+
+
+//    companion object {
+//        private const val TAG = "CameraXBasic"
+//        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+//        private const val REQUEST_CODE_PERMISSIONS = 10
+//        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+//    }
 
 }
